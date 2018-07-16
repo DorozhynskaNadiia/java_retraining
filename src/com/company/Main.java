043808ab16022450c7ab09c075519f4d305110b5
Nadiia <dorozhynska.nadiia@gmail.com>
2018-07-16 20:36:47 +0300

    Correct structure with tabulation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,23 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int size = 3;
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                System.out.print(0 + " ");
+    private static void printRowOf(int length, String symbol)
+    {
+        for (int j = 1; j <= length; j++) {
+            System.out.print(symbol + "\t");
+            if (j == 1) {
+                System.out.print("|\t");
             }
-            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        for (int i = 1; i <= size; i++) {
+            printRowOf(size, "0");
             if (i == 1) {
-                for (int j = 1; j <= size; j++) {
-                    System.out.print("- ");
-                }
-                System.out.println();
+                printRowOf(size, "-");
             }
         }
     }
