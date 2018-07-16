00eaa5af6ea199432069c07c541edde4735c86ae
Nadiia <dorozhynska.nadiia@gmail.com>
2018-07-16 19:55:04 +0300

    basic matrix output implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+        int size = 3;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.print(0 + " ");
+            }
+            System.out.println();
+            if (i == 1) {
+                for (int j = 1; j <= size; j++) {
+                    System.out.print("- ");
+                }
+                System.out.println();
+            }
+        }
     }
+
 }
+
