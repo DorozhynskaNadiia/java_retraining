1fa0d2203607555009b07d162b55eb00f5320fb8
Nadiia <dorozhynska.nadiia@gmail.com>
2018-07-17 20:38:53 +0300

    Added matrix task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,37 @@ package com.company;
 
 public class Main {
 
-    private static void printRowOf(int length, String symbol)
-    {
-        for (int j = 1; j <= length; j++) {
-            System.out.print(symbol + "\t");
-            if (j == 1) {
-                System.out.print("|\t");
+    private static void printCell(int rowIndex, int columnIndex) {
+        int cell;
+        if (rowIndex == 1) {
+            cell = columnIndex;
+        } else {
+            cell = columnIndex * rowIndex;
+        }
+        System.out.print(cell + "\t");
+    }
+
+    private static void printMatrix(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                printCell(i, j);
+                if(j == 1) {
+                    System.out.print("|\t");
+                }
+            }
+            System.out.println();
+
+            if(i == 1) {
+                for(int d = 1; d <=size + 1; d++) {
+                    System.out.print("-\t");
+                }
+                System.out.println();
             }
         }
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        int size = 10;
-        for (int i = 1; i <= size; i++) {
-            printRowOf(size, "0");
-            if (i == 1) {
-                printRowOf(size, "-");
-            }
-        }
+        printMatrix(10);
     }
 
 }
