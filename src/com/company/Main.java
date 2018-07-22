74819877be3f7fed0bbb0c2f4f418d35d52507b5
Nadiia <dorozhynska.nadiia@gmail.com>
2018-07-22 22:52:20 +0300

    Improved printing of '-' and ' ' depending of matrix's rows and columns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        MultiplicationMatrix.printMatrix(10);
+        MultiplicationMatrix.printMatrix(20, 50);
     }
 }
--- src/com/company/MultiplicationMatrix.java
+++ src/com/company/MultiplicationMatrix.java
@@ -2,32 +2,39 @@ package com.company;
 
 public class MultiplicationMatrix {
 
-    private static void printCell(int rowIndex, int columnIndex) {
+    private static void printCell(int rowIndex, int columnIndex, int width) {
         int cell;
         if (rowIndex == 1) {
             cell = columnIndex;
         } else {
             cell = columnIndex * rowIndex;
         }
-        System.out.print(cell + "\t");
+        System.out.format("%" + width + "d", cell);
     }
 
-    public static void printMatrix(int size) {
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                printCell(i, j);
+    public static void printMatrix(int row, int column) {
+        int columnWidth = getColumnWidth(row, column);
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+                printCell(i, j, columnWidth);
                 if(j == 1) {
-                    System.out.print("|\t");
+                    System.out.printf("%s", "|");
                 }
             }
             System.out.println();
 
             if(i == 1) {
-                for(int d = 1; d <=size + 1; d++) {
-                    System.out.print("-\t");
+                for(int k = 1; k <= columnWidth * column + 1; k++) {
+                    System.out.print("-");
                 }
                 System.out.println();
             }
         }
+    }
+
+    private static int getColumnWidth(int rowNum, int columnNum) {
+        int maxValueOfMatrix = rowNum * columnNum;
+        String maxValueString = Integer.toString(maxValueOfMatrix);
+        return maxValueString.length() + 1;
     }
 }
